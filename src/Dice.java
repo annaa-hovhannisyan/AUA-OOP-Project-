10f059ddef4ec63e8080c929236cae376f661b38
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-01 20:49:42 +0400

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,1 +1,20 @@
-
+import java.util.Random;
+public class Dice {
+    private Random random;
+    private int lastRoll1;
+    private int lastRoll2;
+    public Dice() {
+        this.random = new Random();
+    }
+    public int roll() {
+        lastRoll1 = random.nextInt(6) + 1;
+        lastRoll2 = random.nextInt(6) + 1;
+        System.out.println("Rolled: " + lastRoll1 + " + " + lastRoll2 + " = " + (lastRoll1 + lastRoll2));
+        return lastRoll1 + lastRoll2;
+    }
+    public boolean isDoubles() {
+        return lastRoll1 == lastRoll2;
+    }
+    public int getDie1() { return lastRoll1; }
+    public int getDie2() { return lastRoll2; }
+}
