6e0d6e13b3ca2a04ece921919ce7ca77eb1d6d41
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-11 19:31:09 +0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -5,6 +5,7 @@ public class Player {
     private int money;
     private int position;
     private boolean inJail;
+    private int jailTurns;          
     private List<Property> properties;
     private boolean bankrupt;
     public Player(String name, int startingMoney) {
@@ -12,19 +13,16 @@ public class Player {
         this.money = startingMoney;
         this.position = 0;
         this.inJail = false;
+        this.jailTurns = 0;
         this.properties = new ArrayList<>();
         this.bankrupt = false;
     }
     public void move(int steps, int boardSize) {
-        int newPos = (position + steps) % boardSize;
-        // Passed GO
-        if (newPos < position) {
-            addMoney(200);
-            System.out.println(player.getName() + " passed GO! Collect $200");
-        }
-        position = newPos;
+        position = (position + steps) % boardSize;
     }
-    public void addMoney(int amount) { money += amount; }
+    public void addMoney(int amount) {
+        money += amount;
+    }
     public void subtractMoney(int amount) {
         money -= amount;
         if (money <= 0) {
@@ -33,17 +31,51 @@ public class Player {
             System.out.println(name + " is bankrupt!");
         }
     }
-    public void addProperty(Property p) { properties.add(p); }
-    public String getName() { return name; }
-    public int getMoney() { return money; }
-    public int getPosition() { return position; }
-    public void setPosition(int position) { this.position = position; }
-    public boolean isInJail() { return inJail; }
-    public void setInJail(boolean inJail) { this.inJail = inJail; }
-    public boolean isBankrupt() { return bankrupt; }
-    public List<Property> getProperties() { return properties; }
+    public void addProperty(Property p) { 
+        properties.add(p); 
+    }
+    public void goToJail(int jailPosition) {
+        inJail = true;
+        jailTurns = 0;
+        position = jailPosition;
+        System.out.println(name + " is sent to Jail!");
+    }
+    public int getJailTurns() { 
+        return jailTurns; 
+    }
+    public void incrementJailTurns() { 
+        jailTurns++; 
+    }
+    public void releaseFromJail() {
+        inJail = false;
+        jailTurns = 0;
+    }
+    public String getName() { 
+        return name; 
+    }
+    public int getMoney() { 
+        return money; 
+    }
+    public int getPosition() { 
+        return position; 
+    }
+    public void setPosition(int pos) { 
+        this.position = pos; 
+    }
+    public boolean isInJail() { 
+        return inJail; 
+    }
+    public void setInJail(boolean b) { 
+        this.inJail = b; 
+    }
+    public boolean isBankrupt() { 
+        return bankrupt; 
+    }
+    public List<Property> getProperties() { 
+        return properties; 
+    }
     @Override
     public String toString() {
-        return name + " | Money: $" + money + " | Position: " + position;
+        return name + " | Money: $" + money + " | Position: " + position + (inJail ? " [IN JAIL]" : "");
     }
 }
