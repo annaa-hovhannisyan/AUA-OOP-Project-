d7ac4e251a2a9258724e4e3b31e7a3b95f4a9af1
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-11 19:34:51 +0400

    Update GoTile.java

--- src/GoTile.java
+++ src/GoTile.java
@@ -1,12 +1,9 @@
 public class GoTile extends Tile {
-
     public GoTile() {
         super("GO", 0);
     }
-
     @Override
     public void landOn(Player player) {
-        // Landing directly on GO grants a bonus (passing GO is handled in Player.move())
         player.addMoney(200);
         System.out.println(player.getName() + " landed on GO! Collect $200.");
     }
