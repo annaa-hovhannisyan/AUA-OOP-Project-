d35caa9983433ca1e6b0b75cff59e6fa5edea729
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-11 19:43:16 +0400

    Update TaxTile.java

--- src/TaxTile.java
+++ src/TaxTile.java
@@ -1,21 +1,20 @@
 public class TaxTile extends Tile {
-
     private final int taxAmount;
     private final String color;
-
     public TaxTile(String name, int position, int taxAmount, String color) {
         super(name, position);
         this.taxAmount = taxAmount;
         this.color = color;
     }
-
     @Override
     public void landOn(Player player) {
-        System.out.println(player.getName() + " landed on " + getName()
-                + ". Pay $" + taxAmount + " in taxes.");
+        System.out.println(player.getName() + " landed on " + getName() + ". Pay $" + taxAmount + " in taxes.");
         player.subtractMoney(taxAmount);
     }
-
-    public int getTaxAmount() { return taxAmount; }
-    public String getColor()  { return color; }
+    public int getTaxAmount() { 
+        return taxAmount; 
+    }
+    public String getColor() { 
+        return color; 
+    }
 }
