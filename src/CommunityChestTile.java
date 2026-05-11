0adcc6a7ca844c5ba3b89845cc5c55b3906fc2f8
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-11 19:51:19 +0400

    Update CommunityChestTile.java

--- src/CommunityChestTile.java
+++ src/CommunityChestTile.java
@@ -1,32 +1,20 @@
-import java.util.Random;
-
 public class CommunityChestTile extends Tile {
-
-    private static final int[][] CARDS = {
-            { 200, 1},  // Bank error in your favor
-            { 100, 1},  // Doctor's fees refund
-            { 50,  1},  // Sale of stock
-            {-100, -1}, // Pay school fees
-            {-50,  -1}, // Doctor's fees
-            { 150, 1},  // Insurance matures
-    };
-
+    private static final CardDeck deck = new CardDeck("community");
     public CommunityChestTile(String name, int position) {
         super(name, position);
     }
-
     @Override
     public void landOn(Player player) {
         System.out.println(player.getName() + " landed on Community Chest!");
-        Random rand = new Random();
-        int[] card = CARDS[rand.nextInt(CARDS.length)];
-        int amount = card[0];
-        if (amount > 0) {
-            player.addMoney(amount);
-            System.out.println("Community Chest: Collect $" + amount + "!");
-        } else {
-            player.subtractMoney(-amount);
-            System.out.println("Community Chest: Pay $" + (-amount) + ".");
+        CardDeck.Card card = deck.draw();
+        System.out.println("Card: " + card.getDescription());
+        int effect = card.getCashEffect();
+        if (effect > 0) {
+            player.addMoney(effect);
+            System.out.println(player.getName() + " receives $" + effect + ". New balance: $" + player.getMoney());
+        } else if (effect < 0) {
+            player.subtractMoney(Math.abs(effect));
+            System.out.println(player.getName() + " pays $" + Math.abs(effect) + ". New balance: $" + player.getMoney());
         }
     }
 }
