b681ffa5eafd100424c8139b8d6fcee2777b1126
annaa-hovhannisyan <anna_hovhannisyan3@edu.aua.am>
2026-05-11 19:26:22 +0400

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,21 +1,19 @@
-public abstract class Tile{
-  private final String name;
-  private final int position;
-
-  public Tile(String name, int position){
-    this.name = name;
-    this.position = position;
-  }
-  public String getName(){
-    return name;
-  }
-  public int getPosition(){
-    return position;
-  }
-  public abstract void landOn(Player player); //when the player lands on that particular tile
-
-  @Override
-  public String toString(){
-    return name + "(position " + position + ")";
-  }
+public abstract class Tile {
+    private String name;
+    private int position;
+    public Tile(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPosition() {
+        return position;
+    }
+    public abstract void landOn(Player player); 
+    @Override
+    public String toString() {
+        return name + " (position " + position + ")";
+    }
 }
