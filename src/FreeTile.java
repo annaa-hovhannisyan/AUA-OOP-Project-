4ae9bcfe12e2a294907648b18ac3d7b259ea2756
Seda Hovhannisyan <seda_hovhannisyan2@edu.aua.am>
2026-05-14 01:47:39 +0400

    Implement FreeTile class with landOn method

--- src/FreeTile.java
+++ src/FreeTile.java
@@ -1,7 +1,17 @@
+/**
+ * Represents a free/rest tile.
+ * No action happens when landing on it.
+ */
+
 public class FreeTile extends Tile {
     public FreeTile(String name, int position) {
         super(name, position);
     }
+
+    /**
+     * Executes the free tile action.
+     * @param player current player
+     */
     @Override
     public void landOn(Player player) {
         System.out.println(player.getName() + " landed on " + getName() + ". Enjoy the free rest!");
